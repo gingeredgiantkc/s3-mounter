29ccf03a8818fd4afd0181eb31d2a16a622df16a
Jamie <68573208+gingeredgiantkc@users.noreply.github.com>
2020-10-31 15:20:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
-s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd  && tail -f /dev/null
+s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd -p url="$S3_ENDPOINT" && tail -f /dev/null
